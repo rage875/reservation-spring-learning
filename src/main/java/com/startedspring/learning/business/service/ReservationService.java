fa4e32c8fddd844b1ba6819423c2abb06722bb80
rage875 <rage.875@gmail.com>
2021-09-02 18:11:39 -0500

    Service Tier: Service and domain

---
+++ src/main/java/com/startedspring/learning/business/service/ReservationService.java
@@ -0,0 +1,64 @@
+package com.startedspring.learning.business.service;
+
+import com.startedspring.learning.business.domain.RoomReservation;
+import com.startedspring.learning.data.entity.Guest;
+import com.startedspring.learning.data.entity.Reservation;
+import com.startedspring.learning.data.entity.Room;
+import com.startedspring.learning.data.repository.GuestRepository;
+import com.startedspring.learning.data.repository.ReservationRepository;
+import com.startedspring.learning.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ReservationService {
+    private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(
+            RoomRepository roomRepository,
+            GuestRepository guestRepository,
+            ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<RoomReservation> getRoomReservation(Date date) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getNumber());
+
+            roomReservationMap.put(room.getId(), roomReservation);
+        });
+
+        Iterable<Reservation> reservations =
+                this.reservationRepository.findReservationByDate(new java.sql.Date(date.getTime()));
+
+        reservations.forEach(reservation -> {
+            RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
+            roomReservation.setDate(date);
+
+            Guest guest = this.guestRepository.findById(reservation.getGuestId()).get();
+            roomReservation.setFirstName(guest.getFirstName());
+            roomReservation.setLastName(guest.getLastName());
+            roomReservation.setGuestId(guest.getId());
+        });
+
+        List<RoomReservation> roomReservationList = new ArrayList<>();
+        for(Long id: roomReservationMap.keySet()) {
+            roomReservationList.add(roomReservationMap.get(id));
+        }
+
+        return roomReservationList;
+    }
+}
--- src/main/java/com/startedspring/learning/data/repository/ReservationRepository.java
+++ src/main/java/com/startedspring/learning/data/repository/ReservationRepository.java
@@ -2,6 +2,11 @@ package com.startedspring.learning.data.repository;
 
 import com.startedspring.learning.data.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
+@Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByDate(Date date);
 }
