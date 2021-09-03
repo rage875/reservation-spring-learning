2eda50f7e9337fa691f2da29246815702494b740
rage875 <rage.875@gmail.com>
2021-09-03 10:07:17 -0500

    Web pages with Spring: Guest controller, page and redirect

--- src/main/java/com/startedspring/learning/business/service/ReservationService.java
+++ src/main/java/com/startedspring/learning/business/service/ReservationService.java
@@ -73,4 +73,24 @@ public class ReservationService {
 
         return roomReservationList;
     }
+
+    public List<Guest> getHotelGuests() {
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        List<Guest> guestList = new ArrayList<>();
+        guests.forEach(guest -> {
+            guestList.add(guest);
+        });
+        guestList.sort(new Comparator<Guest>() {
+            @Override
+            public int compare(
+                    Guest o1,
+                    Guest o2) {
+                if (o1.getLastName() == o2.getLastName()) {
+                    return o1.getFirstName().compareTo(o2.getFirstName());
+                }
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        });
+        return guestList;
+    }
 }
--- src/main/java/com/startedspring/learning/web/controller/GuestController.java
+++ src/main/java/com/startedspring/learning/web/controller/GuestController.java
@@ -1,20 +1,29 @@
 package com.startedspring.learning.web.controller;
 
+import com.startedspring.learning.business.service.ReservationService;
 import com.startedspring.learning.data.entity.Guest;
-import com.startedspring.learning.data.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping("/guests")
 public class GuestController {
+    private final ReservationService reservationService;
+
     @Autowired
-    private GuestRepository repository;
+    public GuestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping
-    public Iterable<Guest> getGuests() {
-        return this.repository.findAll();
+    public String getGuests(Model model) {
+        List<Guest> guests = this.reservationService.getHotelGuests();
+        model.addAttribute("guests", guests);
+        return "guests";
     }
 }
